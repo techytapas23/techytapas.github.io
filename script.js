7b2091b195adb95dc4a5de7695499008ef791ef4
Tapas Kumar Nayak <130275889+techytapas23@users.noreply.github.com>
2024-09-24 11:44:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Script to add smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
